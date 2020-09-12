407da33c9fac38312318fb76197e0a3862b81407
raulikeda <rauligs@insper.edu.br>
2020-09-12 14:41:45 -0300

    django example

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 sudo apt install python3-dev libpq-dev python3-pip -y
 python3 -m pip install -r requirements.txt
 python3 manage.py migrate
+echo '@reboot cd /home/ubuntu/tasks && ./run.sh' | crontab
 export DJANGO_SUPERUSER_PASSWORD=cloud
 export DJANGO_SUPERUSER_USERNAME=cloud
 export DJANGO_SUPERUSER_EMAIL=cloud@a.com
 python3 manage.py createsuperuser --noinput
-echo '@reboot cd /home/ubuntu/tasks && ./run.sh' | crontab
