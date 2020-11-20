b24e77d5e7b632f58ecaca792d59bee0b257f756
Leonardo <leo@arch.localdomain>
2020-11-20 09:28:15 -0300

    Get tasks?

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from models import Task
 from django.http import HttpResponse
 
 
@@ -7,3 +8,22 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
+
+
+#def createTask(request):
+#    if request.method == 'POST':
+#        form = StudentForm(request.POST)
+#        if form.is_valid():
+#            form.save()
+#            return redirect('/data')
+#    else:
+#        form =StudentForm()
+#        context = {
+#            'form':form
+#        }
+#        return render(request,'create.html',context)
+
+
+def getTasks(request):
+    tasks = Task.objects.all()
+    return HttpResponse(tasks)
